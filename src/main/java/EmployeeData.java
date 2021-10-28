9427a99c63435ba0be91dced08828af109b4b5b0
lohade <abhisheklohade8584@gmail.com>
2021-10-28 12:08:59 +0530

    show the tables from databases

--- src/main/java/EmployeeData.java
+++ src/main/java/EmployeeData.java
@@ -1,36 +1,32 @@
 import java.sql.*;
 
 public class EmployeeData {
-
-    /*
     static final String DB_URL = "jdbc:mysql://localhost:3306/payroll_service";
     static final String USER = "root";
     static final String PASS = "abhishek";
-    public  void getData() {
 
+
+    public  void getData()  {
         String sql=("select * from employee_payroll;");
         try(
                 Connection con= DriverManager.getConnection(DB_URL,USER,PASS);
                 Statement stmt=con.createStatement();
                 ResultSet rs=stmt.executeQuery(sql)){
             while (rs.next()) {
-                System.out.println(rs.getString("id")+ "\t" + rs.getString("name")
-                                   + rs.getString("salary") + "\t" + rs.getString("startDate")
-                        + rs.getString("gender") + "\t" + rs.getString("address")
-                        + rs.getString("phone") + "\t" + rs.getString("department")
-                        + rs.getString("basic_pay") + "\t" + rs.getString("deducation")
-                        + rs.getString("income_tax") + "\t" + rs.getString("net_pay")
+                System.out.println(rs.getString("id")+ "\t" + rs.getString("name") + "\t"
+                                   + rs.getString("salary") + "\t" + rs.getString("startDate") + "\t"
+                        + rs.getString("gender") + "\t" + rs.getString("address") + "\t"
+                        + rs.getString("phone") + "\t" + rs.getString("department") + "\t"
+                        + rs.getString("basic_pay") + "\t" + rs.getString("deducation") + "\t"
+                        + rs.getString("income_tax") + "\t" + rs.getString("net_pay") + "\t"
                         + rs.getString("taxable_pay"));
             }
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
-
     public static void main(String[] args){
-        EmployeeData data=new EmployeeData();
-        data.getData();
+        EmployeeData connection=new EmployeeData();
+        connection.getData();
     }
-
-     */
 }
